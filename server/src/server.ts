80c156a67432112408c2c8396a100fab290e4790
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2022-05-06 11:17:53 -0300

    Stage 5 - A descoberta
    Fazendo deploy da aplicação
    front-end e back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,8 @@ import { routes } from './routes';
 
 const app = express();
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors());
 app.use(express.json());
 app.use(routes);
